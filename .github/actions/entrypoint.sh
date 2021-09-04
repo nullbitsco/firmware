7eb947234f727febb3031473231255fe2aaa76e6
Jay Greco <jayv.greco@gmail.com>
2021-09-04 06:40:18 +0000

    Add commit info to releases

--- .github/actions/entrypoint.sh
+++ .github/actions/entrypoint.sh
@@ -1,6 +1,16 @@
 #!/bin/sh -l
 
-git clone --no-checkout https://github.com/qmk/qmk_firmware.git --depth 1 
+GITHASH_STR="<b>Commits used to build this release:</b><br>"
+
+append_githash_info () {
+    GITHASH=$(git rev-parse HEAD)
+    GITHASH_SHORT=$(echo "$GITHASH" | cut -c 1-7)
+    GITNAME=$(basename `git rev-parse --show-toplevel`)
+    GITURL=$(git config --get remote.origin.url)
+    GITHASH_STR="$GITHASH_STR $GITNAME: [$GITHASH_SHORT]($GITURL/commit/$GITHASH)<br>"
+}
+
+git clone --no-checkout https://github.com/qmk/qmk_firmware --depth 1 
 
 cd qmk_firmware
 
@@ -8,8 +18,15 @@ git config core.sparsecheckout true
 echo '/*\n!/keyboards\n/keyboards/nullbitsco/*\n' > .git/info/sparse-checkout
 git checkout --
 
+append_githash_info
+
 cd keyboards/nullbitsco
 git submodule add https://github.com/nullbitsco/tidbit tidbit
+
+cd tidbit
+append_githash_info
+echo "::set-output name=commits::$GITHASH_STR"
+cd ../
 
 cd ../../
 
